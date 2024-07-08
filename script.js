cc45b743656fa97efb7467743658db3078c6730d
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-08 11:02:08 -0300

    feat: Adicionando tipos de dados JS

---
+++ script.js
@@ -0,0 +1,46 @@
+// Tipos de dados em JavaScript (Number, NaN e Math)
+
+// Number
+
+// Inteiros
+
+var num1 = 42;
+var num2 = 42.42;
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// NaN
+
+var num3 = 42 / "dois";
+// console.log(num3);
+
+var num4 = "dois" / 42;
+// console.log(num4);
+
+// Math
+
+var num5 = Math.PI;
+// console.log(num5);
+
+var num6 = Math.round(4.7);
+// console.log(num6);
+
+var num7 = Math.pow(8, 2);
+// console.log(num7);
+
+var num8 = Math.sqrt(64);
+// console.log(num8);
+
+var num9 = Math.abs(-4.7);
+// console.log(num9);
+
+var num10 = Math.ceil(4.4);
+// console.log(num10);
+
+var num11 = Math.round(4.4);
+// console.log(num11);
+
+var num12 = Math.floor(4.7);
+// console.log(num12);
+
